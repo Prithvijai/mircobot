063c82b9e5c114de225b0b470267f7e6b0402abe
Prithvijai <prithvijai0510@gmail.com>
2024-10-22 21:52:13 -0700

    added gazebo plugin , still lidar and camera not working

--- mircobot_description/launch/robot.launch.py
+++ mircobot_description/launch/robot.launch.py
@@ -2,9 +2,11 @@ import os
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch import LaunchDescription
+from launch.substitutions import TextSubstitution
 
 def generate_launch_description():
     display_robot_rviz = IncludeLaunchDescription(
@@ -15,15 +17,43 @@ def generate_launch_description():
     )
         ))   # it contain Robot_state_pub , Joint_state_pub , and rviz
     
+    world_var = DeclareLaunchArgument(
+        name='world',
+        default_value=TextSubstitution(text="empty.world")
+    )
+
     # need to create node for gazebo launch
-
- 
-
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('gazebo_ros'),
+                'launch/gazebo.launch.py'
+            )),
+            launch_arguments={
+                'world': LaunchConfiguration('world'),
+                'pause':'true'
+            }.items()
+        )
+    
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity','mircobot',
+            '-topic','robot_description'
+        ],
+        output='screen'
+    )
+    # The robot is spawned in Gazebo now add control system to the robot and camera and lidar.
 
 
 
     return LaunchDescription([
-        display_robot_rviz
+        display_robot_rviz,
+        world_var,
+        gazebo_launch,
+        spawn_robot
+
 
 
 
